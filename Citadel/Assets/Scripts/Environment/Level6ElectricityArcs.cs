d82c8e18c4e9e0466bce18ed3c9290324ace4669
Michael Berger <michael.anthony.berger@gmail.com>
2020-04-01 15:49:05 -0400

    Finished Level 6 wall-boxing

---
+++ Citadel/Assets/Scripts/Environment/Level6ElectricityArcs.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level6ElectricityArcs : MonoBehaviour
+{
+    public List<GameObject> arcObjects;
+    public float MIN_EXIST_TIME = 0.1f, MAX_EXIST_TIME = 1f;
+
+    private bool isActive = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            StartCoroutine(FireLaser());
+        }
+    }
+
+    private IEnumerator FireLaser()
+    {
+        int arcChosen = Random.Range(0, arcObjects.Count);
+
+        if (!arcObjects[arcChosen].activeSelf)
+        {
+            arcObjects[arcChosen].SetActive(true);
+            yield return new WaitForSeconds(Random.Range(MIN_EXIST_TIME, MAX_EXIST_TIME));
+            arcObjects[arcChosen].SetActive(false);
+        }
+    }
+
+    public void ActivateLasers()
+    {
+        isActive = true;
+    }
+
+    public void DeactivateLasers()
+    {
+        isActive = false;
+    }
+}
---
+++ Citadel/Assets/Scripts/Environment/PlayerDamageTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VHS;
+
+public class PlayerDamageTrigger : MonoBehaviour
+{
+    // Public Properties
+    [Header("General Variables")]
+    [Tooltip("How much damage the player takes when they hit the trigger.")]
+    public float damageAmount = 10;
+    [Tooltip("How long it takes for the trigger to reset before it can damage the player again.")]
+    public float resetTime = 2f;
+
+    // Private Properties
+    private bool hasBeenTriggered = false;
+
+    // OnTriggerStay()
+    private void OnTriggerStay(Collider other)
+    {
+        // IF the other object is the player...
+        if (other.gameObject.layer == 8 && !hasBeenTriggered)
+        {
+            hasBeenTriggered = true;
+            InteractionController.instance.TakeDamage(damageAmount);
+            StartCoroutine(ResetTrigger());
+        }
+    }
+
+    // ResetTrigger()
+    private IEnumerator ResetTrigger()
+    {
+        yield return new WaitForSeconds(resetTime);
+        hasBeenTriggered = false;
+    }
+}
